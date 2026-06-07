04e7f19e88803bd7fa15b2dd5526e7c330effa04
ccrash <cybercrash@hotmail.it>
2026-06-07 04:12:53 +0100

    test fix

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^.+\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+    '^@expo/vector-icons$': '<rootDir>/__mocks__/vectorIconsMock.js',
+    '^@expo/vector-icons/(.*)$': '<rootDir>/__mocks__/vectorIconsMock.js',
   },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|nativewind)',
